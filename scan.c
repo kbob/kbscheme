3dfeecf254778e58d8734aecc2e0940c141e241f
Bob Miller <kbob@jogger-egg.com>
2009-04-03 10:38:30 -0700

    Returns TOK_* constants for all tokens.  Moved unit tests here from yread.y.

--- scan.c
+++ scan.c
@@ -1,11 +1,12 @@
 #include "scan.h"
 
+#include "test.h"
+#include "types.h"
+
 #include <assert.h>
 #include <string.h>
 #include <unicode.h>
 #include <wctype.h>
-
-#include "types.h"
 
 static inline bool is_whitespace(wchar_t wc)
 {
@@ -154,6 +155,7 @@ extern token_type_t yylex(obj_t **lvalp, instream_t *in)
 {
     wint_t wc, w2;
 
+    *lvalp = NIL;
     while ((wc = instream_getwc(in)) != WEOF) {
 	/* XXX implement " <string element>* " */
 	if (is_whitespace(wc))
@@ -164,7 +166,18 @@ extern token_type_t yylex(obj_t **lvalp, instream_t *in)
 	    continue;
 	}
 	if (wcschr(L"()[]", wc)) {
-	    return (token_type_t)wctob(wc);
+	    switch (wc) {
+	    case L'(':
+		return TOK_LPAREN;
+	    case L')':
+		return TOK_RPAREN;
+	    case L'[':
+		return TOK_LBRACKET;
+	    case L']':
+		return TOK_RBRACKET;
+	    default:
+		assert(0);
+	    }
 	}
 	if (wc == L'.') {
 	    /* . is a token.
@@ -177,7 +190,7 @@ extern token_type_t yylex(obj_t **lvalp, instream_t *in)
 		instream_ungetwc(w2, in);
 	    if (!is_ident_subsequent(w2)) {
 		if (n == 1)
-		    return (token_type_t)wctob(L'.');
+		    return TOK_PERIOD;
 		if (n == 3) {
 		    *lvalp = make_symbol(L"...");
 		    return TOK_SIMPLE;
@@ -377,3 +390,139 @@ const char *token_name(token_type_t tok)
 	assert(false && "unknown token type");
     }
 }
+
+/* spaces */
+TEST_READ(L"(a b)",                     L"(a b)");
+TEST_READ(L"(a\tb)",                    L"(a b)");
+TEST_READ(L"(a\vb)",                    L"(a b)");
+TEST_READ(L"(a\fb)",                    L"(a b)");
+TEST_READ(L"(a\rb)",                    L"(a b)");
+TEST_READ(L"(a\x0085"L"b)",             L"(a b)"); /* <next line> */
+TEST_READ(L"(a\x2028"L"b)",             L"(a b)"); /* <line separator> */
+TEST_READ(L"(a\x2029"L"b)",             L"(a b)"); /* <paragraph separator> */
+TEST_READ(L"(a\x00a0"L"b)",             L"(a b)"); /* category Zs */
+TEST_READ(L"(a\x2028"L"b)",             L"(a b)"); /* category Zl */
+TEST_READ(L"(a\x2029"L"b)",             L"(a b)"); /* category Zp */
+
+/* comments */
+TEST_READ(L"(a;comment\nb)",            L"(a b)");
+TEST_READ(L"(a;comment\rb)",            L"(a b)");
+TEST_READ(L"(a;comment\r\nb)",          L"(a b)");
+TEST_READ(L"(a;comment\x0085"L"b)",     L"(a b)");
+TEST_READ(L"(a;comment\r\x0085"L"b)",   L"(a b)");
+TEST_READ(L"(a;comment\r\nb)",          L"(a b)");
+TEST_READ(L"(a;comment\x2028"L"b)",     L"(a b)");
+TEST_READ(L"(a#||#b)",                  L"(a b)");
+TEST_READ(L"(a#|comment|#b)",           L"(a b)");
+TEST_READ(L"(a#|||#b)",                 L"(a b)");
+TEST_READ(L"(a#|#||#|#b)",              L"(a b)");
+TEST_READ(L"(a#|#|comment|#|#b)",       L"(a b)");
+TEST_READ(L"(a#|#||#comment|#b)",       L"(a b)");
+
+/* identifiers */
+#define TEST_IDENT(name)						\
+    TEST_READ(L ## #name, L ## #name);					\
+    TEST_EVAL(L"(symbol? '" L ## #name L")", L"#t");
+TEST_IDENT(a);
+TEST_IDENT(A);
+TEST_IDENT(!);
+TEST_IDENT($);
+TEST_IDENT(%);
+TEST_IDENT(&);
+TEST_IDENT(*);
+TEST_IDENT(/);
+TEST_IDENT(:);
+TEST_IDENT(<);
+TEST_IDENT(=);
+TEST_IDENT(>);
+TEST_IDENT(?);
+TEST_IDENT(^);
+TEST_IDENT(_);
+TEST_IDENT(~);
+TEST_IDENT(\x0102);			/* category Ll */
+TEST_IDENT(\x0101);			/* category Lu */
+TEST_IDENT(\x01c5);			/* category Lt */
+TEST_IDENT(\x02b0);			/* category Lm */
+TEST_IDENT(\x01bb);			/* category Lo */
+TEST_IDENT(\x0300);			/* category Mn */
+TEST_IDENT(\x2163);			/* category Nl */
+TEST_IDENT(\x00bc);			/* category No */
+TEST_IDENT(\x301c);			/* category Pd */
+TEST_IDENT(\x2040);			/* category Pc */
+TEST_IDENT(\x055e);			/* category Po */
+TEST_IDENT(\x0e3f);			/* category Sc */
+TEST_IDENT(\x208a);			/* category Sm */
+TEST_IDENT(\x02c2);			/* category Sk */
+TEST_IDENT(\x0482);			/* category So */
+TEST_IDENT(\xe000);			/* category Co */
+TEST_IDENT(aaA!$%&*/:<=>?^_~);
+TEST_IDENT(abc\x0102);			/* category Ll */
+TEST_IDENT(abc\x0101);			/* category Lu */
+TEST_IDENT(abc\x01c5);			/* category Lt */
+TEST_IDENT(abc\x02b0);			/* category Lm */
+TEST_IDENT(abc\x01bb);			/* category Lo */
+TEST_IDENT(abc\x0300);			/* category Mn */
+TEST_IDENT(abc\x2163);			/* category Nl */
+TEST_IDENT(abc\x00bc);			/* category No */
+TEST_IDENT(abc\x301c);			/* category Pd */
+TEST_IDENT(abc\x2040);			/* category Pc */
+TEST_IDENT(abc\x055e);			/* category Po */
+TEST_IDENT(abc\x0e3f);			/* category Sc */
+TEST_IDENT(abc\x208a);			/* category Sm */
+TEST_IDENT(abc\x02c2);			/* category Sk */
+TEST_IDENT(abc\x0482);			/* category So */
+TEST_IDENT(abc\xe000);			/* category Co */
+TEST_IDENT(a123);
+TEST_IDENT(abc\x0660);			/* category Nd */
+TEST_IDENT(abc\x0903);			/* category Mc */
+TEST_IDENT(abc\x20dd);			/* category Me */
+TEST_IDENT(a+-.@);
+TEST_IDENT(+);
+TEST_IDENT(-);
+TEST_IDENT(...);
+TEST_IDENT(->);
+TEST_IDENT(->abc);
+TEST_READ(L"(->)",			L"(->)");
+TEST_READ(L"\\x61;",			L"a");
+TEST_READ(L"\\X61;\\X3BB;",		L"a\x3bb");
+
+/*from r6rs section 4.2.4 */
+TEST_IDENT(lambda);
+TEST_IDENT(q);
+TEST_IDENT(soup);
+TEST_IDENT(list->vector);
+TEST_IDENT(+);
+TEST_IDENT(V17a);
+TEST_IDENT(<=);
+TEST_IDENT(a34kTMNs);
+TEST_IDENT(->-);
+TEST_IDENT(the-word-recursion-has-many-meanings);
+TEST_READ(L"\\x3BB;",			L"\x3bb");
+
+/* numbers */
+
+#define TEST_NUMBER(input, expected)					\
+    TEST_READ(L ## #input, L ## #expected);					\
+    TEST_EVAL(L"(number? " L ## #expected L")", L"#t");
+TEST_NUMBER(0,       0);
+TEST_NUMBER(+12,    12);
+TEST_NUMBER(-23,   -23);
+//TEST_NUMBER(#i0,   0);
+//TEST_NUMBER(#I0,   0);
+//TEST_NUMBER(#e0,   0);
+//TEST_NUMBER(#E0,   0);
+//TEST_NUMBER(#b101, 5);
+//TEST_NUMBER(#i0,   0);
+//TEST_NUMBER(#i0,   0);
+//TEST_NUMBER(#i0,   0);
+//TEST_NUMBER(#i0,   0);
+//TEST_READ(L"#i0",                       L"0");
+//TEST_READ(L"#I0",                       L"0");
+//TEST_READ(L"#e0",                       L"0");
+//TEST_READ(L"#E0",                       L"0");
+//TEST_READ(L"#b101",                     L"5");
+//TEST_READ(L"#o77",                      L"63");
+//TEST_READ(L"#e#b101",                   L"5");
+//TEST_READ(L"0.1",                       L"0.1");
+//TEST_READ(L"#e0.1",                     L"1/10");
+
--- scan.h
+++ scan.h
@@ -1,26 +1,24 @@
 #ifndef SCAN_INCLUDED
 #define SCAN_INCLUDED
 
-#include <io.h>
-#include <obj.h>
+#include "io.h"
+#include "obj.h"
 
 typedef enum token_type {
-    TOK_EOF,
     TOK_EXACT_NUMBER,
     TOK_SIMPLE,
     TOK_ABBREV,
     TOK_COMMENT,
     TOK_BEGIN_VECTOR,
     TOK_BEGIN_BYTEVECTOR,
-    TOK_LPAREN = L'(',
-    TOK_RPAREN = L')',
-    TOK_PERIOD = L'.',
-    TOK_LBRACKET = L'[',
-    TOK_RBRACKET = L']',
-    /*
-     * These single characters are also legal token types.
-     * ( ) [ ] .
-     */
+    TOK_LPAREN,
+    TOK_RPAREN,
+    TOK_PERIOD,
+    TOK_LBRACKET,
+    TOK_RBRACKET,
+    TOK_EOF,
+
+    TOKEN_TYPE_COUNT
 } token_type_t;
 
 extern token_type_t yylex(obj_t **lvalp, instream_t *in);
