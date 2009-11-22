c2f56c8aa389949d226f607770f67fc1a75d8d4f
Bob Miller <kbob@jogger-egg.com>
2009-11-22 12:13:27 -0800

    Cosmetic.

--- obj_vector.h
+++ obj_vector.h
@@ -7,6 +7,6 @@ extern obj_t *make_vector(size_t len, obj_t *fill);
 extern bool   is_vector(obj_t *);
 extern size_t vector_len(obj_t *);
 extern obj_t *vector_ref(obj_t *, size_t index);
-extern void   vector_set(obj_t *, size_t index, obj_t *elem);
+extern void   vector_set(obj_t *vec, size_t index, obj_t *elem);
 
 #endif /* !OBJ_VECTOR_INCLUDED */
